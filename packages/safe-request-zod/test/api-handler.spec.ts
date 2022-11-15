76294a676a9384a31b885293c4cb3f4f049017f4
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-11-15 22:15:28 +0100

    test: add error tests

--- packages/safe-request-zod/test/api-handler.spec.ts
+++ packages/safe-request-zod/test/api-handler.spec.ts
@@ -1,3 +1,4 @@
+import { HttpBadRequest } from '@belgattitude/http-exception';
 import { z } from 'zod';
 import { safeRequest } from '../src/getSafeRequest';
 
@@ -7,25 +8,44 @@ describe('Api handler tests', () => {
       const exampleRequest = {
         method: 'GET',
         query: {
-          param1: 'world',
+          name: 'world',
+          email: 'test@example.com',
         },
         headers: {
           accept:
             'text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8',
         },
+        cookies: {},
       };
       const safeReq = safeRequest(exampleRequest);
       const { query, headers } = safeReq.parse({
         method: 'GET',
         query: {
-          param1: z.string(),
+          name: z.string(),
+          email: z.string().email(),
         },
         headers: {
           accept: z.string().regex(/^text\/html/),
         },
       });
-      expect(query.param1).toStrictEqual(exampleRequest.query.param1);
+      expect(query).toStrictEqual(exampleRequest.query);
       expect(headers.accept).toStrictEqual(exampleRequest.headers.accept);
     });
+  });
+  describe('when request payload is invalid', () => {
+    const invalidRequest = {
+      method: 'GET',
+      query: {
+        email: 'world',
+      },
+    };
+    const safeReq = safeRequest(invalidRequest);
+    const schema = {
+      method: 'GET',
+      query: {
+        email: z.string().email(),
+      },
+    };
+    expect(() => safeReq.parse(schema)).toThrow(HttpBadRequest);
   });
 });
